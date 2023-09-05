be3cb3f1c07a43cf43e10c90cf780e03f63c5443
derio <derio@derioo.de>
2023-09-05 20:49:20 +0200

    fix(DisableSearchCompletion): added runtime and co

--- src/main/java/de/derioo/annotations/DisableSearchCompletion.java
+++ src/main/java/de/derioo/annotations/DisableSearchCompletion.java
@@ -1,4 +1,11 @@
 package de.derioo.annotations;
 
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Target(ElementType.TYPE)
+@Retention(RetentionPolicy.RUNTIME)
 public @interface DisableSearchCompletion {
 }
