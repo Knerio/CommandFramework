5dd18eab1bf8fae50c98c26bf1992d111541d353
derio <derio@derioo.de>
2023-09-05 11:22:42 +0200

    feat: disabled main class starting

--- src/main/java/de/derioo/Main.java
+++ src/main/java/de/derioo/Main.java
@@ -1,17 +1,19 @@
 package de.derioo;
 
+import org.bukkit.Bukkit;
 import org.bukkit.plugin.java.JavaPlugin;
 
+import java.util.logging.Level;
+
+/**
+ * The main class of the inner plugin, cannot be started
+ */
 public final class Main extends JavaPlugin {
 
     @Override
     public void onEnable() {
-        // Plugin startup logic
-
+        getLogger().log(Level.WARNING, "Inventory Framework cannot be enabled");
+        Bukkit.getPluginManager().disablePlugin(this);
     }
 
-    @Override
-    public void onDisable() {
-        // Plugin shutdown logic
-    }
 }
