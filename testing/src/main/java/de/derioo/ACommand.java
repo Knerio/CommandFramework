6bdd33b43501b11f3e1cf80bb44dcaf5087c0c2a
derio <derio@derioo.de>
2023-09-05 13:28:07 +0200

    feat: Class to register commands and the framework

--- testing/src/main/java/de/derioo/ACommand.java
+++ testing/src/main/java/de/derioo/ACommand.java
@@ -4,12 +4,18 @@ import de.derioo.annotations.Async;
 import de.derioo.annotations.CommandInit;
 import de.derioo.interfaces.Command;
 import de.derioo.objects.CommandBody;
+import org.bukkit.Bukkit;
+import org.bukkit.entity.Player;
+
+import java.util.stream.Collectors;
 
 @CommandInit(name = "test", permission = "perm")
 public class ACommand implements Command {
 
 
-    public void sendHelp(CommandBody body) {
+    @Mapping(args = "{time}{unit} {player}")
+    @TabCompletion(args = "{time}->~1-100~ {unit}->s;m;h {player}->~players~")
+    public void banForDuration(CommandBody body) {
 
     }
 
