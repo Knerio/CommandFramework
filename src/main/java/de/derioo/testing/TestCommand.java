48bf14f380c652b032b3d754b3fd36789aae9571
derio <derio@derioo.de>
2023-09-06 14:48:44 +0200

    depend changed version, chanded test command

--- src/main/java/de/derioo/testing/TestCommand.java
+++ src/main/java/de/derioo/testing/TestCommand.java
@@ -3,23 +3,38 @@ package de.derioo.testing;
 import de.derioo.annotations.CommandProperties;
 import de.derioo.annotations.Mapping;
 import de.derioo.annotations.NeedsNoPlayer;
+import de.derioo.annotations.Possibilities;
 import de.derioo.interfaces.Command;
 import de.derioo.objects.CommandBody;
 import net.kyori.adventure.text.Component;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @CommandProperties(name = "test")
 public class TestCommand extends Command {
 
-    @Mapping(args = " ")
+    @Mapping(args = "")
     @NeedsNoPlayer
     public void root(CommandBody body) {
         body.executor().sendMessage(Component.text("You executed noting hehe"));
     }
 
 
-    @Mapping(args = "1")
+    @Mapping(args = "1 {player}")
+    @Possibilities(args = "{player}->~getPlayers~")
     @NeedsNoPlayer
     public void one(CommandBody body) {
         body.executor().sendMessage(Component.text("You executed noting hehe"));
+    }
+
+    @Mapping(args = "13 {int}")
+    @Possibilities(args = "{int}->~1-20~")
+    public void test(CommandBody body) {
+
+    }
+
+    public List<String> getPlayers() {
+        return List.of("DeRio");
     }
 }
